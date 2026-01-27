c479e4c23561b04db2b19bca686643f78f306462
lemongrass636-oss <lemongrass636@gmail.com>
2026-01-27 20:19:08 +0900

    簡単なaboutとtopページの変更

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,13 @@
+import Link from 'next/link'; // これをインポート
+
 export default function AboutPage() {
   return (
     <div className="p-10">
       <h1 className="text-2xl font-bold">About ページ</h1>
-      <p>これは新しく追加されたページです！</p>
-      <a href="/" className="text-blue-500 underline">トップへ戻る</a>
+      {/* aタグを Linkコンポーネントに書き換える */}
+      <Link href="/" className="text-blue-500 underline">
+        トップへ戻る（爆速！）
+      </Link>
     </div>
   );
 }
