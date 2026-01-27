3a8884eaa192a605410b07a11a69f9f3919b56fd
lemongrass636-oss <lemongrass636@gmail.com>
2026-01-27 20:14:18 +0900

    Aboutページを作る

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+  return (
+    <div className="p-10">
+      <h1 className="text-2xl font-bold">About ページ</h1>
+      <p>これは新しく追加されたページです！</p>
+      <a href="/" className="text-blue-500 underline">トップへ戻る</a>
+    </div>
+  );
+}
